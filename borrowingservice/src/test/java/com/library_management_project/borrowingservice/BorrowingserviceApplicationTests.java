0048bb859f18293f7505b7f719c2e7e8a8bef1c8
pekana16 <pekic.milos11@gmail.com>
2024-10-29 22:38:23 +0100

    Spring initial

---
+++ borrowingservice/src/test/java/com/library_management_project/borrowingservice/BorrowingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_management_project.borrowingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
